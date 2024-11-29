34cf59074c01f47599f521055ec06e56406f7c42
iSeven23 <fabiovizzini48@gmail.com>
2024-11-29 16:06:57 +0100

    first commit

---
+++ src/main/java/org/iSeven23/aio/Aio.java
@@ -0,0 +1,17 @@
+package org.iSeven23.aio;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Aio extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
