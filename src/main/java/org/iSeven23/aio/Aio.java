3472a2c94528169224df87ac08375481f2ffa246
iSeven23 <fabiovizzini48@gmail.com>
2024-12-01 13:09:19 +0100

    implementato il controllo su PlaceholderAPI.jar e inizializzato il comando gms

--- src/main/java/org/iSeven23/aio/Aio.java
+++ src/main/java/org/iSeven23/aio/Aio.java
@@ -1,5 +1,9 @@
 package org.iSeven23.aio;
+import me.clip.placeholderapi.PlaceholderAPI;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.iSeven23.aio.Commands.Gm.gms;
 import org.iSeven23.aio.Utils.Logo;
 import java.io.File;
 
@@ -7,10 +11,23 @@ public final class Aio extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        if (isPluginInstalled("PlaceholderAPI")) {
+            getLogger().info("PlaceholderAPI è installato!");
+        } else {
+            getLogger().warning("PlaceholderAPI non è installato! Alcune funzionalità potrebbero non essere disponibili.");
+        }
+
         new Logo().printlogo();
         createConfig();
+
+        getCommand("gms").setExecutor(new gms(this));
+
     }
 
+    public boolean isPluginInstalled(String pluginName) {
+        Plugin plugin = Bukkit.getPluginManager().getPlugin(pluginName);
+        return plugin != null && plugin.isEnabled();
+    }
     private void createConfig(){
         File pluginFolder = getDataFolder(); // Rappresenta la cartella plugins/<NomePlugin>
         if (!pluginFolder.exists()) {
@@ -24,7 +41,7 @@ public final class Aio extends JavaPlugin {
         File configFile = new File(getDataFolder(), "config.yml"); // Percorso: plugins/<NomePlugin>/config.yml
         if (!configFile.exists()) {
             saveDefaultConfig(); // Copia il file predefinito dalla directory resources
-            getLogger().info("File config.yml creato con i valori predefiniti.");
+            //getLogger().info("File config.yml creato con i valori predefiniti.");
         } else {
             getLogger().info("File config.yml già esistente. Nessuna azione necessaria.");
         }
